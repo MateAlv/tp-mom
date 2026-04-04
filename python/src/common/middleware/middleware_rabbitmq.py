ad10c6d113f7d2718f079067807ad724a6d0952e
Mate <mateoalvarez2012@gmail.com>
2026-04-04 20:27:27 -0300

    declared methods. First queue init impl

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -5,10 +5,43 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
-    def __init__(self, host, queue_name):
+    def __init__(self, host, queue_name): 
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost')
+        )
+
+        self.channel = self.connection.channel()
+
+        self.channel.queue_declare(queue=queue_name)
+
+        
+
+    def send(self, message: bytes):
+        pass
+
+    def start_consuming(self, on_message_callback):
+        pass
+
+    def stop_consuming(self):
+        pass
+
+    def close(self):
         pass
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
         pass
+
+    def send(self, message: bytes):
+        pass
+
+    def start_consuming(self, on_message_callback):
+        pass
+
+    def stop_consuming(self):
+        pass
+
+    def close(self):
+        pass    
+    
